1349c98800ff54087df193d5991653227fe39750
EvaRZ <evaredondo99@alumnos.cei.es>
2024-04-19 17:37:45 +0200

    cambios header excel

--- js/excel.js
+++ js/excel.js
@@ -1,1 +1,11 @@
-excel.js
+const abrir = document.querySelector(".burger");
+const cerrar = document.querySelector(".cerrar");
+const menu = document.querySelector(".menu_hamburguesa");
+
+abrir.addEventListener("click", () => {
+	menu.style.right = "0";
+});
+
+cerrar.addEventListener("click", () => {
+	menu.style.right = "-200px";
+});

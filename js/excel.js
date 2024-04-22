268d6942262e4e1af473dbdac2b32225a7337651
EvaRZ <evaredondo99@alumnos.cei.es>
2024-04-22 17:42:40 +0200

    prototipo pagina de excel con css

--- js/excel.js
+++ js/excel.js
@@ -1,6 +1,8 @@
 const abrir = document.querySelector(".burger");
 const cerrar = document.querySelector(".cerrar");
 const menu = document.querySelector(".menu_hamburguesa");
+const paneles = document.querySelectorAll(".panel");
+const botones = document.querySelectorAll(".numeros button");
 
 abrir.addEventListener("click", () => {
 	menu.style.right = "0";
@@ -9,3 +11,14 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
 	menu.style.right = "-200px";
 });
+
+for(let i = 0; i < botones.length; i++){
+	botones[i].addEventListener("click", () => {
+		for(let j = 0; j < botones.length; j++){
+			botones[j].classList.remove("activo");
+			paneles[j].classList.remove("visible");
+		}
+		botones[i].classList.add("activo");
+		paneles[i].classList.add("visible");
+	});
+}

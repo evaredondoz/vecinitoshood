4ab73d7ea3f69dd76f672d98833e1d04005129cb
EvaRZ <evaredondo99@alumnos.cei.es>
2024-04-23 11:57:15 +0200

    comentarios en el html y js de la home y el excel

--- js/excel.js
+++ js/excel.js
@@ -1,23 +1,58 @@
+/*---------------------------------------------*\
+* excel.js
+* 	Interacciones:
+* 	- Abrir el menú hamburguesa
+* 	- Cerrar el menú hamburguesa
+* 	- Abrir una explicación con su botón correspondiente
+* 	Estructura:
+* 	- Constantes
+* 	- Eventos
+*----------------------------------------------*/
+
+// Elemento del icono del menú hamburguesa
 const abrir = document.querySelector(".burger");
+// Elemento de botón para cerrar el menú hamburguesa
 const cerrar = document.querySelector(".cerrar");
+// Elemento de la modal del menú hamburguesa
 const menu = document.querySelector(".menu_hamburguesa");
+// Selección de todas las explicaciones del Excel
 const paneles = document.querySelectorAll(".panel");
+// Selección de todos los botones que abren las explicaciones del Excel
 const botones = document.querySelectorAll(".numeros button");
 
+/**
+ * Función que desplaza la modal a la izquierda
+ */
 abrir.addEventListener("click", () => {
+	// Desplaza la modal a la izquierda
 	menu.style.right = "0";
 });
 
+/**
+ * Función que desplaza la modal a la derecha
+ */
 cerrar.addEventListener("click", () => {
+	// Desplaza la modal a la derecha
 	menu.style.right = "-200px";
 });
 
+/**
+ * Bucle que abre una explicación con su botón correspondiente
+ */
 for(let i = 0; i < botones.length; i++){
+	/**
+	 * Función que muestra la explicación
+	 */
 	botones[i].addEventListener("click", () => {
+		/**
+		 * Bucle que esconde las explicaciones que no están activas
+		 */
 		for(let j = 0; j < botones.length; j++){
+			// Quita la clase activo y visible del resto de otones y explicaciones
 			botones[j].classList.remove("activo");
 			paneles[j].classList.remove("visible");
 		}
+		// Añade la clase activo y visible a la explicación y botón seleccionado.
 		botones[i].classList.add("activo");
 		paneles[i].classList.add("visible");
 	});
--- js/home.js
+++ js/home.js
@@ -1,22 +1,57 @@
-const perfiles = document.querySelectorAll(".perfiles div");
-const botones = document.querySelectorAll(".perfiles button");
+/*---------------------------------------------*\
+* home.js
+* 	Interacciones:
+* 	- Abrir el menú hamburguesa
+* 	- Cerrar el menú hamburguesa
+* 	- Abrir un perfil con su botón correspondiente
+* 	Estructura:
+* 	- Constantes
+* 	- Eventos
+*----------------------------------------------*/
+
+// Elemento del icono del menú hamburguesa
 const abrir = document.querySelector(".burger");
+// Elemento de botón para cerrar el menú hamburguesa
 const cerrar = document.querySelector(".cerrar");
+// Elemento de la modal del menú hamburguesa
 const menu = document.querySelector(".menu_hamburguesa");
+// Selección de todos los perfiles de Instagram
+const perfiles = document.querySelectorAll(".perfiles div");
+// Selección de todo los botones que muestran los perfiles de Instagra
+const botones = document.querySelectorAll(".perfiles button");
 
-botones.forEach((boton,i) => {
-	boton.addEventListener("click", () => {
-		for(j = 0; j < perfiles.length; j++){
-			perfiles[j].classList.remove("visible");
-		}
-		perfiles[i].classList.add("visible");
-	});
-});
-
+/**
+ * Función que desplaza la modal a la izquierda
+ */
 abrir.addEventListener("click", () => {
+	// Desplaza la modal a la izquierda
 	menu.style.right = "0";
 });
 
+/**
+ * Función que desplaza la modal a la derecha
+ */
 cerrar.addEventListener("click", () => {
+	// Desplaza la modal a la derecha
 	menu.style.right = "-200px";
+});
+
+/**
+ * Bucle que abre los perfiles dependiendo del botón seleccionado
+ */
+botones.forEach((boton,i) => {
+	/**
+	 * Función que abre los perfiles con su botón correspondiente
+	 */
+	boton.addEventListener("click", () => {
+		/**
+		 * Bucle que cierra los perfiles no seleccionados
+		 */
+		for(j = 0; j < perfiles.length; j++){
+			// Quita la clase visible de los perfiles no seleccionados
+			perfiles[j].classList.remove("visible");
+		}
+		// Añade la lase visible en el perfil seleccionado
+		perfiles[i].classList.add("visible");
+	});
 });

2c67eda220ef563dff4af96a42d29e3af53efc6c
EvaRZ <evaredondo99@alumnos.cei.es>
2024-04-28 16:57:52 +0200

    página de reglas sin grid

--- js/rules.js
+++ js/rules.js
@@ -1,6 +1,11 @@
 const abrir = document.querySelector(".burger");
 const cerrar = document.querySelector(".cerrar");
 const menu = document.querySelector(".menu_hamburguesa");
+const botones = document.querySelectorAll("section button");
+const paneles = document.querySelectorAll(".reglas");
+const visible = document.getElementsByClassName("visible");
+const altura = [];
+
 
 abrir.addEventListener("click", () => {
 	menu.style.right = "0";
@@ -9,3 +14,17 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
 	menu.style.right = "-200px";
 });
+
+for(let i = 0; i < botones.length; i++){
+	altura.push(paneles[i].clientHeight + "px");
+	paneles[i].style.height = 0;
+	botones[i].addEventListener("click", () => {
+		if(paneles[i].classList.contains("visible")){
+			paneles[i].style.height = 0;
+			paneles[i].classList.remove("visible");
+		}else{
+			paneles[i].style.height = altura[i];
+			paneles[i].classList.add("visible");
+		}
+	});
+}
